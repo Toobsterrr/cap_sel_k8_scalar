8004599f4882409c4a62fec3a1518daa3f67245b
tobias <tobiaslamote@gmail.com>
2024-04-13 13:10:56 +0200

    saas-app: add distinct delete scripts

---
+++ saas-app/kube_deployment/delete_gold.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+sla="limitrange_gold"
+namespace=`sed -e 's#.*_\(\)#\1#' <<< ${sla}`
+echo SLA configuration ${namespace}
+echo ==============================
+# echo $sla
+# echo $namespace
+# exit
+
+
+kubectl delete -f resourcequota/compute-resources.yaml --namespace=${namespace}
+kubectl delete -f slas/${sla}/limits.yaml --namespace=${namespace}
+kubectl delete -f deploy_saas_kube.yaml --namespace=${namespace}
+kubectl delete -f expose.yaml --namespace=${namespace}
+kubectl delete -f slas/${sla}/namespace.yaml
+
